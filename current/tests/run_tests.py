97dc55b35e15c41c47364b36c555ce81fdb69725
Mesut Karakoç <mkarakoc@users.noreply.github.com>
2022-07-06 20:28:16 +0300

    cleaned

--- current/tests/run_tests.py
+++ current/tests/run_tests.py
@@ -1,15 +1,7 @@
 import os
+from bifold import print_title
 
-def print_title(title='', n_l='\n', omit=''):
-    l_title = len(title)
-    sides = (110-l_title)//2
-    if sides>1:
-        side_strip = '═' * (sides-1)
-        print(f'{omit}{n_l}{omit}{side_strip}╣{title}╠{side_strip}{omit}{n_l}{omit}')
-    else:
-        print(f'{omit}{n_l}{omit}{title}')
-
-working_dir = os.getcwd()
+workdir = os.getcwd()
 for dirpath, dnames, fnames in os.walk("./"):
     for fname in fnames:
         if fname.endswith(".py") and 'run_test' not in fname:
@@ -17,4 +9,4 @@ for dirpath, dnames, fnames in os.walk("./"):
             print_title(title=test_file)
             os.chdir(dirpath)
             os.system(f'python {fname}')
-            os.chdir(working_dir)
+            os.chdir(workdir)
